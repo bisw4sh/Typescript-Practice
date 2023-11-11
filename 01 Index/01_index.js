b7b2c3e82256cb11051719418649aecebd6de8be
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-11-11 19:03:12 +0545

    clean js files & gitignore

--- 01 Index/01_index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let helloWorld = "Hello World";
-const user = {
-    name: "Hayes",
-    id: 0,
-};
-console.log(user);
--- 02 Add/02_add.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const add = (a, b) => a + b;
-console.log(add(5, 6));
