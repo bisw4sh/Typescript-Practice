789fd68394361ea43eed8410d6c057866d7373ac
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-11-12 22:19:07 +0545

    enums

---
+++ 07 Enums/07_enums.ts
@@ -0,0 +1,12 @@
+enum Capital {
+  India = "New Delhi",
+  USA = "Washington",
+  Germany = "Berlin",
+  England = "London",
+}
+
+console.log("The values initialized by string enum is as follows:");
+console.log(Capital.India);
+console.log(Capital.USA);
+console.log(Capital.Germany);
+console.log(Capital.England);
