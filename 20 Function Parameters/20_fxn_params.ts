1684cb683117226104bb6b17f6cb3a3717b988a3
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-11-15 22:24:45 +0545

    function parameters

---
+++ 20 Function Parameters/20_fxn_params.ts
@@ -0,0 +1,10 @@
+// * Vague 
+function foon1(flagA: boolean, flagB: boolean) {
+  // your awesome function body
+}
+
+//TODO: Precise One
+function foon2(config: { flagA: boolean; flagB: boolean }) {
+  const { flagA, flagB } = config;
+  // your awesome function body
+}
